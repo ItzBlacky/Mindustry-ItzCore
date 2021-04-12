3eadae76a163789bed6fe11525bdb9f160c1837e
ItzBlacky <vnwlif0@gmail.com>
2021-04-12 15:48:19 +0700

    Initial Release

---
+++ src/src/me/itzblacky/ItzCore/ItzCore.java
@@ -0,0 +1,46 @@
+package me.itzblacky.ItzCore;
+
+
+import arc.Events;
+import me.itzblacky.ItzCore.Utils.ClassFinder;
+import mindustry.Vars;
+
+import mindustry.game.EventType;
+import mindustry.mod.Mod;
+import mindustry.mod.Mods;
+import mindustry.mod.Plugin;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+public class ItzCore extends Plugin {
+
+    private Map<Class<?>, Mod> apiProviders;
+    private List<Class<?>> providerType;
+
+    @Override
+    public void init() {
+        apiProviders = new HashMap<>();
+        providerType = ClassFinder.findInterface("me.itzblacky.ItzCore.DependencyInterface");
+
+        Events.on(EventType.ServerLoadEvent.class, event -> {
+            Mods mods = Vars.mods;
+            for (Mods.LoadedMod mod : mods.list()) {
+                for (Class<?> clas : providerType) {
+                    if (!clas.isAssignableFrom(mod.main.getClass())) continue;
+
+                    apiProviders.put(clas, mod.main);
+                }
+            }
+        });
+    }
+    public <T> T getProvider(String providerName) {
+        Class<?> clazz = null;
+        for(Class<?> clas : providerType)
+            clazz = clas.getSimpleName().equals(providerName) && clazz == null ? clas : null;
+        Object o = providerType.get(1).cast(apiProviders.get(clazz));
+    }
+
+}
